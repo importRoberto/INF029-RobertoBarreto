97dff5d03e0f93ca658e877c5a7bc0e5204e7c23
Roberto Barreto <robertoboficial@gmail.com>
2025-04-04 21:13:59 -0300

    Update animais.c

--- animais.c
+++ animais.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 #define TAM 3
 
-int main(){ 
-    int idade[TAM];
-    char sexo[TAM];
-    
-    // fazendo a leitura dos dados
-    for (int i = 0; i < TAM; i++){
-        printf("Digite a idade: ");
-        scanf("%d", &idade[i]);
-        getchar();
-        printf("Digite a sexo: ");
-        scanf("%c", &sexo[i]);
+void menu {
+    printf("1 - Cadastrar\n2 - Listar\n3 - Media\n");
+    scanf("%d", &menu);
+    switch menu{
+        case 1: Cadastrar(); break;
+        case 2: Listar(); break;
+        case 3: Media(); break;
+        case 4: exit;
     }
 
-    //imprimindo todos
+void Cadastrar(){
+    int idade[];
+    char sexo[];
+    printf("Digite a idade: ");
+    scanf("%d", &idade[i]);
+    getchar();
+    printf("Digite a sexo: ");
+    scanf("%c", &sexo[i]);
+}
+
+void Listar(){
     printf("#### Todos animais\n");
     for (int i = 0; i < TAM; i++){
         printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
     }
-    
+}
+
+void Media(){
     // calcular a média
     int soma = 0;
-    for (int i = 0; i < TAM; i++){
+    for (int i = 0; i < TAM; i++)
         soma += idade[i];
-    }
+
     int media = soma / TAM;
 
     //imprimindo maiores que a média
@@ -33,4 +43,13 @@ int main(){
         if (idade[i] > media)
             printf("Idade: %d - Sexo: %c\n", idade[i], sexo[i]);
     }
+    
+}
+
+    
+}
+
+int main(){ 
+    
+
 }
